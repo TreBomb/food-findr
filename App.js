9429b9d18761b6d69bd6c7993381538aa595f2b4
Trestin <trestinishak@gmail.com>
2022-01-25 21:43:32 -0600

    restyled screen

--- App.js
+++ App.js
@@ -1,13 +1,14 @@
 import { StatusBar as ExpoStatusBar } from "expo-status-bar";
 import React from "react";
 import { StatusBar, StyleSheet, SafeAreaView, Text, View } from "react-native";
+import { Searchbar } from "react-native-paper";
 
 export default function App() {
   return (
     <>
       <SafeAreaView style={styles.container}>
         <View style={styles.search}>
-          <Text>Search</Text>
+          <Searchbar placeholder="Search" />
         </View>
         <View style={styles.list}>
           <Text>List</Text>
@@ -22,14 +23,15 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     marginTop: StatusBar.currentHeight,
+    backgroundColor: "#fff",
   },
   search: {
     padding: 16,
-    backgroundColor: "#00ff00",
+    backgroundColor: "#fff",
   },
   list: {
     flex: 1,
     padding: 16,
-    backgroundColor: "#0000ff",
+    backgroundColor: "#fff",
   },
 });
