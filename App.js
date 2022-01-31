b90daeb7c8b0852a6e5e8fd5c32a2a1bfa9aabe8
TreBomb <trestinishak@gmail.com>
2022-01-31 14:27:06 -0600

    updated tab options

--- App.js
+++ App.js
@@ -18,16 +18,33 @@ import { theme } from "./src/infrastructure/theme";
 
 const Tab = createBottomTabNavigator();
 
+const TAB_ICON = {
+  Restaurants: "md-restaurant",
+  Map: "md-map",
+  Settings: "md-settings",
+};
+
 const Settings = () => (
   <SafeArea>
     <Text>Settings</Text>
   </SafeArea>
 );
+
 const Map = () => (
   <SafeArea>
     <Text>Map</Text>
   </SafeArea>
 );
+
+const createScreenOptions = ({ route }) => {
+  const iconName = TAB_ICON[route.name];
+  return {
+    tabBarIcon: ({ size, color }) => (
+      <Ionicons name={iconName} size={size} color={color} />
+    ),
+    headerShown: false,
+  };
+};
 
 export default function App() {
   const [oswaldLoaded] = useOswald({
@@ -43,7 +60,13 @@ export default function App() {
     <>
       <ThemeProvider theme={theme}>
         <NavigationContainer>
-          <Tab.Navigator>
+          <Tab.Navigator
+            screenOptions={createScreenOptions}
+            tabBarOptions={{
+              activeTintColor: "tomato",
+              inactiveTintColor: "gray",
+            }}
+          >
             <Tab.Screen name="Restaurants" component={RestaurantsScreen} />
             <Tab.Screen name="Map" component={Map} />
             <Tab.Screen name="Settings" component={Settings} />
