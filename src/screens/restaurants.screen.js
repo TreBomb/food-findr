c2cd8f05f760904a65a26f655fd2155180850e41
TreBomb <trestinishak@gmail.com>
2022-02-01 13:58:07 -0600

    added loading bar

--- src/screens/restaurants.screen.js
+++ src/screens/restaurants.screen.js
@@ -2,6 +2,7 @@ import React, { useContext } from "react";
 import styled from "styled-components";
 import { FlatList, View } from "react-native";
 import { Searchbar } from "react-native-paper";
+import { ActivityIndicator, Colors } from "react-native-paper";
 
 import { SafeArea } from "../components/safe-area.component";
 import { Spacer } from "../components/spacer.component";
@@ -18,11 +19,26 @@ const RestaurantList = styled(FlatList).attrs({
   },
 })``;
 
+const Loading = styled(ActivityIndicator)`
+  margin-left: -25px;
+`;
+
+const LoadingContainer = styled(View)`
+  position: absolute;
+  top: 50%;
+  left 50%;
+`;
+
 export const RestaurantsScreen = () => {
   const { isLoading, error, restaurants } = useContext(RestaurantsContext);
 
   return (
     <SafeArea>
+      {isLoading && (
+        <LoadingContainer>
+          <Loading size={50} animating={true} color={Colors.blue300} />
+        </LoadingContainer>
+      )}
       <SearchContainer>
         <Searchbar placeholder="Search" />
       </SearchContainer>
