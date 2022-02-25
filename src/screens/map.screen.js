ba3ad2ee879f2c5cfd44d5dc9480d759be20da19
TreBomb <trestinishak@gmail.com>
2022-02-24 21:00:54 -0600

    set ios to show google maps

--- src/screens/map.screen.js
+++ src/screens/map.screen.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react";
-import MapView from "react-native-maps";
+import MapView, { PROVIDER_GOOGLE } from "react-native-maps";
 import { useContext } from "react/cjs/react.development";
 import styled from "styled-components";
 import { ActivityIndicator } from "react-native-paper";
@@ -54,6 +54,7 @@ const RestaurantMap = ({ navigation }) => {
         </LoadingContainer>
       )}
       <Map
+        provider={PROVIDER_GOOGLE}
         region={{
           latitude: lat,
           longitude: lng,
