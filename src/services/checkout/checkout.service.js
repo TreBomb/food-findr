73ededdff381556c154f0740339ea5bb7ac61203
TreBomb <trestinishak@gmail.com>
2022-02-26 14:02:51 -0600

    modified checkout service

--- src/services/checkout/checkout.service.js
+++ src/services/checkout/checkout.service.js
@@ -1,21 +1,24 @@
 import createStripe from "stripe-client";
 import { host } from "../../utils/env";
-export const stripe = () =>
-  createStripe(
-    "pk_test_51KX89UJ5zyidjFOHqJ51qnfFUn5UkGZ5hV6d1INMDK0p1Gh2DzJ1GGP7kfLhloWXtHAQH534kG4VJqDUyJc2B0wU00OHiCKiQT"
-  );
 
-export const CardTokenRequest = (card) => stripe.createToken({ card });
+const stripe = createStripe(
+  "pk_test_51KX89UJ5zyidjFOHqJ51qnfFUn5UkGZ5hV6d1INMDK0p1Gh2DzJ1GGP7kfLhloWXtHAQH534kG4VJqDUyJc2B0wU00OHiCKiQT"
+);
 
-export const PayRequest = (token, amount, name) => {
+export const cardTokenRequest = (card) => stripe.createToken({ card });
+
+export const payRequest = (token, amount, name) => {
   return fetch(`${host}/pay`, {
-    body: JSON.stringify({ token, amount, name }),
+    body: JSON.stringify({
+      token,
+      name,
+      amount,
+    }),
     method: "POST",
   }).then((res) => {
     if (res.status > 200) {
-      return Promise.reject("Something went wrong");
-    } else {
-      return res.json();
+      return Promise.reject("something went wrong processing your payment");
     }
+    return res.json();
   });
 };
