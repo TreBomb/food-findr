6e99e0ebabcb2692a2b2d68c14dab01cbe35a5f9
TreBomb <trestinishak@gmail.com>
2022-02-26 13:42:08 -0600

    removing react-native-credit-card-input

--- src/components/credit-card.component.js
+++ src/components/credit-card.component.js
@@ -1,21 +1,49 @@
+// import React from "react";
+// import { LiteCreditCardInput } from "react-native-credit-card-input";
+// import { CardTokenRequest } from "../services/checkout/checkout.service";
+
+// export const CreditCardInput = ({ name, onSuccess, onError }) => {
+//   const onChange = async (formData) => {
+//     const { values, status } = formData;
+//     const isIncomplete = Object.values(status).includes("incomplete");
+//     const expiry = values.expiry.split("/");
+//     const card = {
+//       number: values.number,
+//       exp_month: expiry[0],
+//       exp_year: expiry[1],
+//       cvc: values.cvc,
+//       name: name,
+//     };
+
+//     if (!isIncomplete) {
+//       try {
+//         const info = await CardTokenRequest(card);
+//         onSuccess(info);
+//       } catch (e) {
+//         onError();
+//       }
+//     }
+//   };
+//   return <LiteCreditCardInput onChange={onChange} />;
+// };
+
 import React from "react";
-import { LiteCreditCardInput } from "react-native-credit-card-input";
+import { CardField } from "@stripe/stripe-react-native";
 import { CardTokenRequest } from "../services/checkout/checkout.service";
 
 export const CreditCardInput = ({ name, onSuccess, onError }) => {
   const onChange = async (formData) => {
-    const { values, status } = formData;
-    const isIncomplete = Object.values(status).includes("incomplete");
-    const expiry = values.expiry.split("/");
+    const { complete, expiryMonth, expiryYear } = formData;
+    // const isIncomplete = Object.values(status).includes("incomplete");
     const card = {
-      number: values.number,
-      exp_month: expiry[0],
-      exp_year: expiry[1],
-      cvc: values.cvc,
+      number: "4242 4242 4242 4242",
+      exp_month: expiryMonth,
+      exp_year: expiryYear,
+      cvc: "242",
       name: name,
     };
 
-    if (!isIncomplete) {
+    if (complete) {
       try {
         const info = await CardTokenRequest(card);
         onSuccess(info);
@@ -24,5 +52,29 @@ export const CreditCardInput = ({ name, onSuccess, onError }) => {
       }
     }
   };
-  return <LiteCreditCardInput onChange={onChange} />;
+
+  return (
+    <CardField
+      postalCodeEnabled={false}
+      placeholder={{
+        number: "4242 4242 4242 4242",
+      }}
+      cardStyle={{
+        backgroundColor: "#FFFFFF",
+        textColor: "#000000",
+      }}
+      style={{
+        width: "100%",
+        height: 50,
+        marginVertical: 30,
+      }}
+      onCardChange={(cardDetails) => {
+        console.log("cardDetails", cardDetails);
+        onChange(cardDetails);
+      }}
+      onFocus={(focusedField) => {
+        console.log("focusField", focusedField);
+      }}
+    />
+  );
 };
