66408be873bda98d7c0da155f5143a9c16cdd782
TreBomb <trestinishak@gmail.com>
2022-02-26 14:06:57 -0600

    modified checkout styles

--- src/screens/checkout.screen.js
+++ src/screens/checkout.screen.js
@@ -5,7 +5,7 @@ import { List } from "react-native-paper";
 import { SafeArea } from "../components/safe-area.component";
 import { CreditCardInput } from "../components/credit-card.component";
 import { CartContext } from "../services/cart/cart.context";
-import { PayRequest } from "../services/checkout/checkout.service";
+import { payRequest } from "../services/checkout/checkout.service";
 
 import { Text } from "../components/text.component";
 import { Spacer } from "../components/spacer.component";
@@ -20,7 +20,7 @@ import {
 } from "../styles/checkout.styles";
 
 export const CheckoutScreen = ({ navigation }) => {
-  const { cart, clearCart, restaurant, sum } = useContext(CartContext);
+  const { cart, restaurant, clearCart, sum } = useContext(CartContext);
   const [name, setName] = useState("");
   const [card, setCard] = useState(null);
   const [isLoading, setIsLoading] = useState(false);
@@ -30,22 +30,22 @@ export const CheckoutScreen = ({ navigation }) => {
     if (!card || !card.id) {
       setIsLoading(false);
       navigation.navigate("Checkout Error", {
-        error: "Please enter a valid card",
+        error: "Please fill in a valid credit card",
       });
-    } else {
-      PayRequest(card.id, sum, name)
-        .then((result) => {
-          setIsLoading(false);
-          clearCart();
-          navigation.navigate("Checkout Success");
-        })
-        .catch((error) => {
-          setIsLoading(false);
-          navigation.navigate("Checkout Error", {
-            error: error,
-          });
-        });
+      return;
     }
+    payRequest(card.id, sum, name)
+      .then((result) => {
+        setIsLoading(false);
+        clearCart();
+        navigation.navigate("Checkout Success");
+      })
+      .catch((err) => {
+        setIsLoading(false);
+        navigation.navigate("Checkout Error", {
+          error: err,
+        });
+      });
   };
 
   if (!cart.length || !restaurant) {
@@ -53,26 +53,23 @@ export const CheckoutScreen = ({ navigation }) => {
       <SafeArea>
         <CartIconContainer>
           <CartIcon icon="cart-off" />
-          <Text>Your Cart Is Empty!</Text>
+          <Text>Your cart is empty!</Text>
         </CartIconContainer>
       </SafeArea>
     );
   }
-
   return (
     <SafeArea>
-      {isLoading && <PaymentProcessing />}
       <RestaurantInfoCard restaurant={restaurant} />
+      {isLoading && <PaymentProcessing />}
       <ScrollView>
-        <Spacer position="top" size="large">
-          <Text>Your Order</Text>
-        </Spacer>
         <Spacer position="left" size="medium">
+          <Spacer position="top" size="large">
+            <Text>Your Order</Text>
+          </Spacer>
           <List.Section>
             {cart.map(({ item, price }) => {
-              return (
-                <List.Item key={item} title={`${item} - ${price / 100}`} />
-              );
+              return <List.Item title={`${item} - ${price / 100}`} />;
             })}
           </List.Section>
           <Text>Total: {sum / 100}</Text>
@@ -80,29 +77,32 @@ export const CheckoutScreen = ({ navigation }) => {
         <NameInput
           label="Name"
           value={name}
-          onChangeText={(txt) => {
-            setName(txt);
+          onChangeText={(t) => {
+            setName(t);
           }}
         />
-        {name.length > 0 && (
-          <CreditCardInput
-            name={name}
-            onSuccess={setCard}
-            onError={() =>
-              navigation.navigate("Checkout Error", {
-                error: "Something went wrong processing your credit card",
-              })
-            }
-          />
-        )}
+        <Spacer position="top" size="large">
+          {name.length > 0 && (
+            <CreditCardInput
+              name={name}
+              onSuccess={setCard}
+              onError={() =>
+                navigation.navigate("Checkout Error", {
+                  error: "Something went wrong processing your credit card",
+                })
+              }
+            />
+          )}
+        </Spacer>
         <Spacer position="top" size="xxl" />
+
         <PayButton
           disabled={isLoading}
           icon="cash-usd"
           mode="contained"
           onPress={onPay}
         >
-          Purchase Order
+          Pay
         </PayButton>
         <Spacer position="top" size="large">
           <ClearButton
--- src/utils/env.js
+++ src/utils/env.js
@@ -3,9 +3,9 @@ import { Platform } from "react-native";
 const liveHost = "https://us-central1-foodfindr-ac4b8.cloudfunctions.net";
 const localHost = "http://localhost:5001/foodfindr-ac4b8/us-central1";
 
-const OVERRIDE = true;
+const OVERRIDE = false;
 export const isAndroid = Platform.OS === "android";
 export const isDevelopment = process.env.NODE_ENV === "development";
-export const isMock = false;
+export const isMock = true;
 const baseCheck = !isDevelopment || isAndroid ? true : false;
 export const host = baseCheck || OVERRIDE ? liveHost : localHost;
